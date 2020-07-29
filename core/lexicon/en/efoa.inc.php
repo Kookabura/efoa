e4e2c4d2507e72597d0afee58b2cd7d0bafccf1d
Анатолий Серёгин <aseregin@outlook.com>
2020-07-29 17:10:55 +0300

    Added lexicons for efoa user profile fields

---
+++ core/lexicon/en/efoa.inc.php
@@ -0,0 +1,36 @@
+<?php
+// Lexicon for extended user fields for EFOA
+
+$_lang['user_biography'] = 'Biography';
+$_lang['user_cv'] = 'CV';
+$_lang['user_discipline'] = 'Discipline';
+$_lang['user_facebook'] = 'Facebook';
+$_lang['user_instagram'] = 'Instagram';
+$_lang['user_linkedin'] = 'LinkedIn';
+$_lang['user_openhouse_exhibiting'] = 'Openhouse Exhibiting';
+$_lang['user_openhouse_host'] = 'Openhouse Host';
+$_lang['user_openhouse_number'] = 'Openhouse Number';
+$_lang['user_pinterest'] = 'Pinterest';
+$_lang['user_shop'] = 'Shop';
+$_lang['user_summary'] = 'Summary';
+$_lang['user_twitter'] = 'Twitter';
+$_lang['user_youtube'] = 'Youtube';
+$_lang['user_highlight_image'] = 'Highlight Image';
+
+
+$_lang['user_biography_desc'] = 'Biography';
+$_lang['user_cv_desc'] = 'CV';
+$_lang['user_discipline_desc'] = 'Discipline';
+$_lang['user_facebook_desc'] = 'Facebook';
+$_lang['user_instagram_desc'] = 'Instagram';
+$_lang['user_linkedin_desc'] = 'LinkedIn';
+$_lang['user_openhouse_exhibiting_desc'] = 'Openhouse Exhibiting';
+$_lang['user_openhouse_host_desc'] = 'Openhouse Host';
+$_lang['user_openhouse_number_desc'] = 'Openhouse Number';
+$_lang['user_pinterest_desc'] = 'Pinterest';
+$_lang['user_shop_desc'] = 'Shop';
+$_lang['user_summary_desc'] = 'Summary';
+$_lang['user_twitter_desc'] = 'Twitter';
+$_lang['user_youtube_desc'] = 'Youtube';
+$_lang['user_highlight_image_desc'] = 'Highlight Image';
+$_lang['user_zip_uk'] = 'Post code';
