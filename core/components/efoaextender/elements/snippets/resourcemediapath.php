e4538736ccc655f1126e284f91413f2587a80dfb
Анатолий Серёгин <aseregin@outlook.com>
2020-10-12 13:31:03 +0300

    Improved dynamic media source to work with user's fields.

--- core/components/efoaextender/elements/snippets/resourcemediapath.php
+++ core/components/efoaextender/elements/snippets/resourcemediapath.php
@@ -2,30 +2,23 @@
 /**
  * @name ResourceMediaPath
  * @description Dynamically calculates the upload path for a given resource
- * 
+ *
  * This Snippet is meant to dynamically calculate your baseBath attribute
  * for custom Media Sources.  This is useful if you wish to shepard uploaded
- * images to a folder dedicated to a given resource.  E.g. page 123 would 
+ * images to a folder dedicated to a given resource.  E.g. page 123 would
  * have its own images that page 456 could not reference.
  *
  * USAGE:
- * [[ResourceMediaPath? &pathTpl=`assets/businesses/{id}/`]]
- * [[ResourceMediaPath? &pathTpl=`assets/test/{breadcrumb}`]]
- * [[ResourceMediaPath? &pathTpl=`assets/test/{breadcrumb}` &breadcrumbdepth=`2`]]
+ * [[ResourceMediaPath? &pathTpl=`assets/test/{username}`]]
  *
  * PARAMETERS
- * &pathTpl string formatting string specifying the file path. 
+ * &pathTpl string formatting string specifying the file path.
  *		Relative to MODX base_path
- *		Available placeholders: {id}, {pagetitle}, {parent}
- * &docid (optional) integer page id
+ *		Available placeholders: {usename}, {user.fullname}
  * &createFolder (optional) boolean whether or not to create
- * &forTemplates (optional) number id templates for create folder
  */
 $pathTpl = $modx->getOption('pathTpl', $scriptProperties, '');
-$docid = $modx->getOption('docid', $scriptProperties, '');
 $createfolder = $modx->getOption('createFolder', $scriptProperties, false);
-$tvname = $modx->getOption('tvname', $scriptProperties, '');
-$fortemplates = $modx->getOption('forTemplates', $scriptProperties, '');
 
 $path = '';
 $createpath = false;
@@ -35,107 +28,35 @@ if (empty($pathTpl)) {
     return;
 }
 
-if (empty($docid) && $modx->getPlaceholder('mediasource_docid')) {
-    // placeholder was set by some script
-    // warning: the parser may not render placeholders, e.g. &docid=`[[*parent]]` may fail
-    $docid = $modx->getPlaceholder('mediasource_docid');
-}
+$path = $pathTpl;
+$ultimateParent = '';
 
-if (empty($docid) && $modx->getPlaceholder('docid')) {
-    // placeholder was set by some script
-    // warning: the parser may not render placeholders, e.g. &docid=`[[*parent]]` may fail
-    $docid = $modx->getPlaceholder('docid');
-}
-if (empty($docid)) {
+if ($modx->context->key == 'mgr') {
 
-    //on frontend
-    if (is_object($modx->resource)) {
-        $docid = $modx->resource->get('id');
-    }
-    //on backend
-    else {
-        $createpath = $createfolder;
-        // We do this to read the &id param from an Ajax request
-        $parsedUrl = parse_url($_SERVER['HTTP_REFERER']);
-        parse_str($parsedUrl['query'], $parsedQuery);
-
-        if (isset($parsedQuery['amp;id'])) {
-            $docid = (int)$parsedQuery['amp;id'];
-        } elseif (isset($parsedQuery['id'])) {
-            $docid = (int)$parsedQuery['id'];
-        }
-    }
-}
-
-if (empty($docid)) {
-    $modx->log(MODX_LOG_LEVEL_ERROR, '[ResourceMediaPath]: docid could not be determined.');
-    return;
-}
-
-if ($resource = $modx->getObject('modResource', $docid)) {
-    $path = $pathTpl;
-    $ultimateParent = '';
-    if (strstr($path, '{breadcrumb}') || strstr($path, '{ultimateparent}')) {
-        $depth = $modx->getOption('breadcrumbdepth', $scriptProperties, 10);
-        $ctx = $resource->get('context_key');
-        $parentids = $modx->getParentIds($docid, $depth, array('context' => $ctx));
-        $breadcrumbdepth = $modx->getOption('breadcrumbdepth', $scriptProperties, count($parentids));
-        $breadcrumbdepth = $breadcrumbdepth > count($parentids) ? count($parentids) : $breadcrumbdepth;
-        if (count($parentids) > 1) {
-            $parentids = array_reverse($parentids);
-            $parentids[] = $docid;
-            $ultimateParent = $parentids[1];
-        } else {
-            $ultimateParent = $docid;
-            $parentids = array();
-            $parentids[] = $docid;
-        }
-    }
-
-    if (strstr($path, '{breadcrumb}')) {
-        $breadcrumbpath = '';
-        for ($i = 1; $i <= $breadcrumbdepth; $i++) {
-            $breadcrumbpath .= $parentids[$i] . '/';
-        }
-        $path = str_replace('{breadcrumb}', $breadcrumbpath, $path);
-    }
-    
-    if (!empty($tvname)){
-        $path = str_replace('{tv_value}', $resource->getTVValue($tvname), $path);    
-    }
-    $path = str_replace('{id}', $docid, $path);
-    $path = str_replace('{pagetitle}', $resource->get('pagetitle'), $path);
-    $path = str_replace('{alias}', str_replace('-','.',$resource->get('alias')), $path);
-    $path = str_replace('{parent}', $resource->get('parent'), $path);
-    $path = str_replace('{context_key}', $resource->get('context_key'), $path);
-    $path = str_replace('{ultimateparent}', $ultimateParent, $path);
-    if ($templatename = $resource->getOne('Template')) {
-        $path = str_replace('{templatename}', $templatename->get('templatename'), $path);
-    }
     if ($user = $modx->user) {
-        $path = str_replace('{username}', $modx->user->get('username'), $path);
-    }
-
-    $fullpath = $modx->getOption('base_path') . $path;
-	$template = $resource->get('template');
-	if($fortemplates !== ''){
-		$fortemplates = array_map('trim',explode(',',$fortemplates));
-		if(!in_array($template, $fortemplates)){
-			$createpath = false;
-		}
-	}
-    if ($createpath && !file_exists($fullpath)) {
-
-        $permissions = octdec('0' . (int)($modx->getOption('new_folder_permissions', null, '755', true)));
-        if (!@mkdir($fullpath, $permissions, true)) {
-            $modx->log(MODX_LOG_LEVEL_ERROR, sprintf('[ResourceMediaPath]: could not create directory %s).', $fullpath));
+        if ($profile = $modx->user->getOne('Profile')) {
+            $path = str_replace('{username}', $modx->user->get('username'), $path);
+            $path = str_replace('{user.fullname}', str_replace(['-', ' '],'.',strtolower($profile->get('fullname'))), $path);
         } else {
-            chmod($fullpath, $permissions);
+            $path = 'assets/profiles/';
         }
+    } else {
+        $path = 'assets/profiles/';
     }
-
-    return $path;
 } else {
-    $modx->log(MODX_LOG_LEVEL_ERROR, sprintf('[ResourceMediaPath]: resource not found (page id %s).', $docid));
-    return;
+    $path = str_replace('{user.fullname}', str_replace(['-', ' '],'.',strtolower($modx->resource->get('alias'))), $path);
 }
+
+$fullpath = $modx->getOption('base_path') . $path;
+
+if ($createfolder && !file_exists($fullpath)) {
+
+    $permissions = octdec('0' . (int)($modx->getOption('new_folder_permissions', null, '755', true)));
+    if (!@mkdir($fullpath, $permissions, true)) {
+        $modx->log(MODX_LOG_LEVEL_ERROR, sprintf('[ResourceMediaPath]: could not create directory %s).', $fullpath));
+    } else {
+        chmod($fullpath, $permissions);
+    }
+}
+
+return $path;
